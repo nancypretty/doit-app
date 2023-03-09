caad4efd10f25599d88c000fe14df0eca1533b53
nancypretty <nancypgenia@gmail.com>
2023-03-09 18:10:58 +0700

    functionable todo-list done

--- app.js
+++ app.js
@@ -29,16 +29,25 @@ app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/doitDB");
 
+const todoSchema = new mongoose.Schema ({
+    content: String
+});
+
+
 const userSchema = new mongoose.Schema ({
     username: String,
     password: String,
     googleId: String,
+    todos: [todoSchema],
+    notes: []
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
+const Todo = mongoose.model("Todo", todoSchema);
 const User = new mongoose.model("User", userSchema);
+
 
 passport.use(User.createStrategy());
 
@@ -52,6 +61,8 @@ passport.deserializeUser(function(id, done) {
     });
   });
 
+  // --------- Google Authentication OAUTH20  ---------
+
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -64,36 +75,124 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get("/auth/google/main", 
+  passport.authenticate('google', { failureRedirect: "/" }),
+  function(req, res) {
+    res.redirect('/main');
+  });
+
+
+// :::::::::::::::::::::::::::::::::::  C R U D ::::::::::::::::::::::::::::::::::::::::::::: //
+
 
 
 // Notes: "/" = Login Route
+// ************* LOGIN ROUTE **********
 
-// --------- Username & Password Authentication (GET)  ---------
-app.get("/", function (req, res) {
+app.route("/")
+.get(function (req, res) {
     res.render("login");
+})
+.post(function (req, res) {
+    const user = new User ({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+        passport.authenticate("local", {failureRedirect: "/register"})(req, res, function() {
+            res.redirect("/main");
+        });
+        };
+    });
 });
 
-app.get("/register", function (req, res) {
+
+// ************* REGISTER ROUTE **********
+app.route("/register")
+.get(function (req, res) {
     res.render("register", {message: req.flash('errorMessage')});
+})
+.post(function(req, res) {
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            req.flash('errorMessage', "This username already exist, try another one!");
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+            res.redirect("/main");
+            });
+        };
+    });
 });
 
-app.get("/main", function(req, res) {
+
+// ************* MAIN ROUTE **********
+
+
+app.route("/main")
+.get(async (req, res) => {
+    // Motivational Quotes Generator API
+    var quotes = "";
+    var author = "";
+
+    await fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+    const totalQuotes = data.length;
+    const i = Math.floor(Math.random() * totalQuotes);
+    quotes = data[i].text;
+    author = data[i].author;
+    });
+
     if (req.isAuthenticated()) {
-        fetch("https://type.fit/api/quotes")
-        .then(function(response) {
-        return response.json();
-        })
-        .then(function(data) {
-        const totalQuotes = data.length;
-        const i = Math.floor(Math.random() * totalQuotes);
-        const quotes = data[i].text;
-        const author = data[i].author;
-        res.render("main", {quotes: quotes, author: author} );
-        });    
+        User.findById(req.user.id, function(err, foundUser) {
+            const todoContent = foundUser.todos;
+            res.render("main", {quotes: quotes, author: author, todoList: todoContent} );
+        });
     } else {
         res.redirect("/");
     };
+})
+.post(function(req, res) {
+    const newTodo = new Todo ({
+        content: req.body.newTodo
+    });
+
+    User.findById(req.user.id, function(err, foundUser) {
+        foundUser.todos.push(newTodo);
+        foundUser.save();
+        res.redirect("/main");
+    } );
+    
 });
+
+// ************* MAIN ROUTE **********
+
+app.post("/delete", function(req, res) {
+    const checkedTodo = req.body.checkbox;
+
+    User.findByIdAndUpdate(req.user.id,
+        {$pull: {todos: {_id: checkedTodo}}},
+        function(err, done) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/main");
+            };
+        });
+});
+
+// ************* OTHER ROUTE **********
 
 app.get("/create", function(req, res) {
     res.render("create");
@@ -108,52 +207,6 @@ app.get("/logout", function(req, res) {
         }
     })
 });
-
-// --------- Username & Password Authentication (POST)  ---------
-
-app.post("/", function (req, res) {
-    const user = new User ({
-        username: req.body.username,
-        password: req.body.password
-    });
-
-    req.login(user, function(err, user) {
-        if (err) {
-            console.log(err);
-        } else {
-        passport.authenticate("local", {failureRedirect: "/register"})(req, res, function() {
-        res.redirect("/main");
-        });
-        };
-    });
-});
-
-app.post("/register", function(req, res) {
-    User.register({username: req.body.username}, req.body.password, function(err, user) {
-        if (err) {
-            console.log(err);
-            req.flash('errorMessage', "This username already exist, try another one!");
-            res.redirect("/register");
-        } else {
-            passport.authenticate("local")(req, res, function() {
-            res.redirect("/main");
-            });
-        };
-    });
-});
-
-// --------- Google Authentication  ---------
-
-app.get("/auth/google",
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get("/auth/google/main", 
-  passport.authenticate('google', { failureRedirect: "/" }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/main');
-  });
-
 
 
 
